66349c3aa89748ba5ba825314bc8d76d77987ce1
franktronicsprog <franktronicsprog@gmail.com>
2026-02-18 08:03:18 +0100

    feat: add waitlist endpoint

--- app/controllers/waitlist_controller.ts
+++ app/controllers/waitlist_controller.ts
@@ -33,9 +33,21 @@ export default class WaitlistController {
             ],
           },
           date: {
-            date: {
-              start: new Date().toISOString(),
-            },
+            rich_text: [
+              {
+                text: {
+                  content: new Date().toLocaleString('en-GB', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    hour12: false,
+                  }),
+                },
+              },
+            ],
           },
         },
       })
--- config/shield.ts
+++ config/shield.ts
@@ -17,7 +17,7 @@ const shieldConfig = defineConfig({
    */
   csrf: {
     enabled: true,
-    exceptRoutes: [],
+    exceptRoutes: ['/waitlist'],
     enableXsrfCookie: true,
     methods: ['POST', 'PUT', 'PATCH', 'DELETE'],
   },
